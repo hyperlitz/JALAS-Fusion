8e8f27a9dbbbcf97bf2c12c215dbe1c356743ec8
Lito Banano <135684502+hyperlitz@users.noreply.github.com>
2023-11-25 21:29:42 -0800

    upteded

--- server.js
+++ server.js
@@ -1,50 +1,47 @@
 import express from 'express';
-import colors from 'colors';
+import cors from 'cors';
 import dotenv from 'dotenv';
-import morgon from 'morgan';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
-import authRouter from './Routers/authRouter.js'
-import categoryRouter from './Routers/categoryRouter.js'
-import productRouter from './Routers/productRouter.js'
-import cors from 'cors'
+import authRouter from './Routers/authRouter.js';
+import categoryRouter from './Routers/categoryRouter.js';
+import productRouter from './Routers/productRouter.js';
 
 // configure env
 dotenv.config();
 
-// databse config
-connectDB() 
+// database config
+connectDB();
 
 // rest object
 const app = express();
 
-// middle wires
-app.use(express.json());  
-app.use(express.urlencoded({extended:true})); 
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
-app.use(cors({
-    origin: 'https://jalas-fusion-6b3afa890e65.herokuapp.com',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  }));
-  
+
+// CORS configuration
+const corsOptions = {
+  origin: 'https://jalas-fusion-6b3afa890e65.herokuapp.com',
+  credentials: false, // Do not require credentials
+};
+
+app.use(cors(corsOptions));
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/category', categoryRouter);
-app.use('/api/v1/product', productRouter)
- 
-  
-// rest api 
-app.get("/api", (req, res) => { 
-    res.send("<h1>Welcome</h1>")
-}) 
- 
+app.use('/api/v1/product', productRouter);
+
+// rest API
+app.get('/api', (req, res) => {
+  res.send('<h1>Welcome</h1>');
+});
 
 // PORT
+const PORT = process.env.PORT || 8080;
 
-const PORT = process.env.PORT;
-   
 // run listen
 app.listen(PORT, () => {
-    console.log(`Server is Runnig on The ${PORT}`.bgBlue.white);
-})
+  console.log(`Server is Running on Port ${PORT}`);
+});
